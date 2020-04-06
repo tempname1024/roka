2df5addacd82ac7463ff6d3ec6754b21dab71737
Jordan <me@jordan.im>
2020-04-05 20:20:41 -0700

    initial commit

---
+++ rebuild.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import hashlib
+import json
+import math
+import os
+from datetime import timedelta
+from flask import Flask
+from lib.tinytag import TinyTag
+
+def get_books(root_path):
+    '''
+    Discover audiobooks under :root_path: and populate books object
+    '''
+    if not os.path.exists(root_path):
+        raise ValueError('root path does not exist: %s' % root_path)
+
+    SIZES = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+    _books = dict()
+    book_dirs = list()
+    for root, dirs, _ in os.walk(root_path):
+        for d in dirs:
+            book_dirs.append(os.path.join(root, d))
+
+    for book_path in book_dirs:
+        print('[+] processing: %s' % book_path)
+
+        # initial set of attributes to be populated
+        book = {
+            'duration': 0,
+            'path': book_path,
+            'files': dict(),
+            'size_bytes': 0,
+            'size_str': None,
+        }
+
+        # hash of each file in directory w/ MP3 extension
+        folder_hash = hashlib.md5()
+        is_book = False
+
+        # a book_path is only a book if it contains at least one MP3
+        for f in os.listdir(book_path):
+            file_path = os.path.join(book_path, f)
+            if not os.path.isfile(file_path) or not f.endswith('.mp3'):
+                continue
+
+            # update folder hash with MD5 of current file
+            BLOCK = 1024
+            file_hash = hashlib.md5()
+            with open(file_path, 'rb') as f:
+                while True:
+                    data = f.read(BLOCK)
+                    if not data:
+                        break
+                    folder_hash.update(data)
+                    file_hash.update(data)
+
+            # skip if no duration attribute (required)
+            tag = TinyTag.get(file_path)
+            if not tag.duration:
+                continue
+            is_book = True
+
+            # populate file-specific attributes
+            attr = dict()
+            attr['path'] = file_path
+            attr['duration'] = tag.duration
+            if tag.title:
+                attr['title'] = tag.title
+            else:
+                attr['title'] = file_path.split('/')[-1]
+            if tag.album:
+                attr['album'] = tag.album
+                book['title'] = tag.album
+            else:
+                attr['album'] = book_path.split('/')[-1]
+                book['title'] = book_path.split('/')[-1]
+            if tag.artist:
+                attr['author'] = tag.artist
+                book['author'] = tag.artist
+            else:
+                attr['author'] = 'Unknown'
+                book['author'] = 'Unknown'
+
+            attr['duration'] = tag.duration
+            attr['track'] = tag.track
+            attr['size_bytes'] = tag.filesize
+
+            duration_str = str(timedelta(seconds=attr['duration']))
+            attr['duration_str'] = duration_str.split('.')[0]
+
+            book['duration'] += tag.duration
+            book['files'][file_hash.hexdigest()] = attr
+            book['size_bytes'] += tag.filesize
+
+        if is_book:
+            folder_hash = folder_hash.hexdigest()
+
+            total_size = book['size_bytes']
+            try:
+                _i = int(math.floor(math.log(total_size, 1024)))
+                _p = math.pow(1024, _i)
+                _s = round(total_size / _p, 2)
+            except:
+                _i = 1
+                _s = 0
+
+            # e.g. 1.48 GB
+            book['size_str'] = '%s %s' % (str(_s), SIZES[_i])
+
+            # e.g. 2 days, 5:47:47
+            duration_str = str(timedelta(seconds=book['duration']))
+            book['duration_str'] = duration_str.split('.')[0]
+
+            _books[folder_hash] = book
+
+    return _books
+
+def write_cache(books, json_path):
+    '''
+    Dump contents of :books: to :json_path:
+    '''
+    cache_path = os.path.dirname(json_path)
+    if not os.path.exists(cache_path):
+        os.mkdir(cache_path)
+    with open(json_path, 'w') as f:
+        json.dump(books, f, indent=4)
+
+if __name__ == '__main__':
+    ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+    CACHE_PATH = os.path.join(ABS_PATH, 'cache')
+    JSON_PATH = os.path.join(CACHE_PATH, 'audiobooks.json')
+
+    # use Flask's config parser, configparser would be hacky
+    APP = Flask(__name__)
+    APP.config.from_pyfile(os.path.join(ABS_PATH, 'app.cfg'))
+
+    BOOKS = get_books(APP.config['ROOT_PATH'])
+    write_cache(BOOKS, JSON_PATH)
---
+++ run.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import json
+import mimetypes
+import os
+import re
+import xml.etree.cElementTree as ET
+from datetime import date, timedelta
+from flask import Flask, request, Response, render_template, send_file
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__)
+app.config.from_pyfile(os.path.join(abs_path, 'app.cfg'))
+cache_path = os.path.join(abs_path, 'cache')
+json_path = os.path.join(cache_path, 'audiobooks.json')
+
+# populate books object from JSON cache
+if os.path.exists(json_path):
+    try:
+        with open(json_path, 'r') as cache:
+            books = json.load(cache)
+    except Exception:
+        raise ValueError('error loading JSON cache')
+else:
+    raise ValueError('cache not found, run rebuild.py')
+
+def check_auth(username, password):
+    '''
+    Authenticate against configured user/pass
+    '''
+    ret = (username == app.config['USERNAME'] and
+           password == app.config['PASSWORD'])
+
+    return ret
+
+@app.route('/')
+def list_books():
+    '''
+    Book listing and audiobook RSS/file download
+
+    :a: audiobook hash; if provided without :f: (file) return RSS
+    :f: file hash; requires associated audiobook (:a:) to download
+
+    Listing of audiobooks returned if no params provided
+    '''
+    a = request.args.get('a') # audiobook hash
+    f = request.args.get('f') # file hash
+
+    # audiobook and file parameters provided: serve up file
+    if a and f:
+        if not books.get(a) or not books[a]['files'].get(f):
+            return 'book or file not found', 404
+
+        f_path = books[a]['files'][f]['path']
+
+        # ship the whole file if we don't receive a Range header
+        range_header = request.headers.get('Range', None)
+        if not range_header:
+            return send_file(
+                f_path,
+                mimetype=mimetypes.guess_type(f_path)[0]
+            )
+
+        # partial request handling--certain podcast apps (iOS) and browsers
+        # (Safari) require correct replies to Range requests; if we serve the
+        # entire file, we're treated like a stream (no seek, duration...)
+        size = books[a]['files'][f]['size_bytes']
+
+        # if no lower bound provided, start at beginning
+        byte1, byte2 = 0, None
+        m = re.search(r'(\d+)-(\d*)', range_header)
+        g = m.groups()
+        if g[0]:
+            byte1 = int(g[0])
+        if g[1]:
+            byte2 = int(g[1])
+
+        # if no upper bound provided, serve rest of file
+        length = size - byte1
+        if byte2 is not None:
+            length = byte2 - byte1
+
+        # read file at byte1 for length
+        data = None
+        with open(f_path, 'rb') as f:
+            f.seek(byte1)
+            data = f.read(length)
+
+        # create response with partial data, populate Content-Range
+        response = Response(
+            data,
+            206,
+            mimetype=mimetypes.guess_type(f_path)[0],
+            direct_passthrough=True
+        )
+        response.headers.add(
+            'Content-Range',
+            'bytes {0}-{1}/{2}'.format(byte1, byte1 + length, size)
+        )
+        response.headers.add('Accept-Ranges', 'bytes')
+
+        return response
+
+    # serve up audiobook RSS feed; only audiobook hash provided
+    elif a:
+        if not books.get(a):
+            return 'book not found', 404
+
+        # we only make use of the itunes ns, others provided for posterity
+        namespaces = {
+            'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
+            'googleplay':'http://www.google.com/schemas/play-podcasts/1.0',
+            'atom':'http://www.w3.org/2005/Atom',
+            'media':'http://search.yahoo.com/mrss/',
+            'content':'http://purl.org/rss/1.0/modules/content/',
+        }
+
+        rss = ET.Element('rss')
+        for k, v in namespaces.items():
+            rss.set('xmlns:%s' % k, v)
+        rss.set('version', '2.0')
+
+        channel = ET.SubElement(rss, 'channel')
+
+        book_title = ET.SubElement(channel, 'title')
+        book_title.text = books[a]['title']
+
+        # sort by track number, alphanumerically if track is absent
+        track_list = [] # account for duplicates
+        for a_file in books[a]['files']:
+            track = books[a]['files'][a_file]['track']
+            if not track or track in track_list:
+                key = lambda x: books[a]['files'][x]['title']
+                break
+            track_list.append(track)
+        else:
+            key = lambda x: books[a]['files'][x]['track']
+
+        # populate XML attribute values required by Apple podcasts
+        for idx, f in enumerate(sorted(books[a]['files'], key=key)):
+            item = ET.SubElement(channel, 'item')
+
+            title = ET.SubElement(item, 'title')
+            title.text = books[a]['files'][f]['title']
+
+            author = ET.SubElement(item, 'itunes:author')
+            author.text = books[a]['files'][f]['author']
+
+            category = ET.SubElement(item, 'itunes:category')
+            category.text = 'Book'
+
+            explicit = ET.SubElement(item, 'itunes:explicit')
+            explicit.text = 'no'
+
+            summary = ET.SubElement(item, 'itunes:summary')
+            summary.text = 'Audiobook served by audiobook-rss'
+
+            description = ET.SubElement(item, 'description')
+            description.text = 'Audiobook served by audiobook-rss'
+
+            duration = ET.SubElement(item, 'itunes:duration')
+            duration.text = str(books[a]['files'][f]['duration_str'])
+
+            guid = ET.SubElement(item, 'guid')
+            guid.text = f # file hash
+
+            # pubDate descending, day decremented w/ each iteration
+            pub_date = ET.SubElement(item, 'pubDate')
+            pub_date.text = (date(2000, 12, 31) - timedelta(days=idx)).ctime()
+            enc_attr = {
+                'url': '{}?a={}&f={}'.format( request.base_url, a, f),
+                'length': str(books[a]['files'][f]['size_bytes']),
+                'type': 'audio/mpeg'
+            }
+            ET.SubElement(item, 'enclosure', enc_attr)
+
+        return Response(
+            ET.tostring(rss, encoding='utf8', method='xml'),
+            mimetype='text/xml'
+        )
+    else:
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            form = {'WWW-Authenticate': 'Basic realm="o/"'}
+            return Response('unauthorized', 401, form)
+        return render_template('index.html', books=books)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='8085', threaded=True)
