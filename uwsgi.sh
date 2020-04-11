a1aff012d039832ea2a6c1fc23d1e7a5840f0784
Jordan <me@jordan.im>
2020-04-10 18:18:16 -0700

    increase uwsgi limits in example, venv location-agnostic

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # sandbox dir is virtualenv (python3 -m venv sandbox)
-. sandbox/bin/activate
+# . sandbox/bin/activate
 
 # uwsgi.ini.example provided as template
 uwsgi --ini uwsgi.ini
